6e4e55c0243ca8b19ed4caff66c9168f1568f950
heidar84 <46946357+heidar84@users.noreply.github.com>
2022-02-10 23:57:29 -0800

    Added hello python script.

---
+++ hello.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# HeidarRahmanian
+# CPSC 386-01
+# 2022-02-08
+# Heidar84@csu.fullerton.edu
+# @github
+#
+# Lab 01
+#
+# This first program is a 'Hello World!' program as practice.
+#
+
+
+"""Below this point is the main code. Everything above is header."""
+
+
+def main():
+    """This main function contains the Hello World phrase!"""
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
